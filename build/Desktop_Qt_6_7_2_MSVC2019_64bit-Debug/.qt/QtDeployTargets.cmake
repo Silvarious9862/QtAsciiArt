5ce9d0b3bd9c251e9cc72f63ff963fb12d2b3f6e
Silvarious9862 <vbobrik0@gmail.com>
2024-09-05 13:27:54 +0300

    v2.0

---
+++ build/Desktop_Qt_6_7_2_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_qt_asciiart_FILE D:/cppprog/qt_asciiart/build/Desktop_Qt_6_7_2_MSVC2019_64bit-Debug/qt_asciiart.exe)
+set(__QT_DEPLOY_TARGET_qt_asciiart_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_qt_asciiart_RUNTIME_DLLS C:/Qt/6.7.2/msvc2019_64/bin/Qt6Widgetsd.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.2/msvc2019_64/bin/Qt6Cored.dll)
